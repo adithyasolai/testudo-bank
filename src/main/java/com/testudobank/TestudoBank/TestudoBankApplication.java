0c65135664650dcbead932ea814019220d8b6dd1
Adithya Solai <ppalanic@terpmail.umd.edu>
2021-09-06 19:29:10 -0400

    empty Spring Web war packaging repo

---
+++ src/main/java/com/testudobank/TestudoBank/TestudoBankApplication.java
@@ -0,0 +1,13 @@
+package com.testudobank.TestudoBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestudoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestudoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testudobank/TestudoBank/TestudoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testudobank.TestudoBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestudoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
