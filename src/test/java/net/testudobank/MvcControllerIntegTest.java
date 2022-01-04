a80e5b9eff7959e8fd2b19517da9eaee8bf06b6f
Adithya Solai <ppalanic@terpmail.umd.edu>
2022-01-04 12:45:46 -0500

    change filepath name from codejava to testudobank

--- src/test/java/net/testudobank/MvcControllerIntegTest.java
+++ src/test/java/net/testudobank/MvcControllerIntegTest.java
@@ -1,4 +1,4 @@
-package net.codejava;
+package net.testudobank;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- src/test/java/net/testudobank/MvcControllerTest.java
+++ src/test/java/net/testudobank/MvcControllerTest.java
@@ -1,10 +1,11 @@
-package net.codejava;
+package net.testudobank;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
+
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -26,7 +27,7 @@ public class MvcControllerTest {
   @Mock
   Model mockModel;
 
-	private MvcController controller;
+  private MvcController controller;
 
   private static String CUSTOMER1_USERNAME;
   private static List<Map<String, Object>> CUSTOMER1_DATA;
