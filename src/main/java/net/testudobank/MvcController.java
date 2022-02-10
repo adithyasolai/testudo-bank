fc80665f659f4931f1a9054d782d779f21347daf
mlopez122 <mlopez10@terpmail.umd.edu>
2022-02-09 22:56:07 -0500

    Incorporated helper method into submitWithdraw; new helper declutters explicit multiplication and moves applying interest to pennies to a helper method

--- src/main/java/net/testudobank/MvcController.java
+++ src/main/java/net/testudobank/MvcController.java
@@ -183,6 +183,11 @@ public class MvcController {
     return dateTime;
   }
 
+  // Assignment 1: Applies Interest rate to given penny amount
+  private static int applyInterestRateToPennyAmount(int pennyAmount) {
+    return (int) (pennyAmount * INTEREST_RATE);
+  }
+
   // HTML POST HANDLERS ////
 
   /**
@@ -339,7 +344,7 @@ public class MvcController {
     int userOverdraftBalanceInPennies = TestudoBankRepository.getCustomerOverdraftBalanceInPennies(jdbcTemplate, userID);
     if (userWithdrawAmtInPennies > userBalanceInPennies) { // if withdraw amount exceeds main balance, withdraw into overdraft with interest fee
       int excessWithdrawAmtInPennies = userWithdrawAmtInPennies - userBalanceInPennies;
-      int newOverdraftIncreaseAmtAfterInterestInPennies = (int)(excessWithdrawAmtInPennies * INTEREST_RATE);
+      int newOverdraftIncreaseAmtAfterInterestInPennies = applyInterestRateToPennyAmount(excessWithdrawAmtInPennies);
       int newOverdraftBalanceInPennies = userOverdraftBalanceInPennies + newOverdraftIncreaseAmtAfterInterestInPennies;
 
       // abort withdraw transaction if new overdraft balance exceeds max overdraft limit
